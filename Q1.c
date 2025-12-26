2c426426f6386c4f3da07e1817232dbc9cb92749
Nayan <nayanworkspace@gmail.com>
2025-12-26 11:18:54 +0530

    First Code

---
+++ Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int arr[3][3] = { {1, 2, 3},
+                      {4, 5, 6},
+                      {7, 8, 9} };
+    int (*ptr)[3] = arr;
+
+    printf("Elements of the 2D array accessed through pointer:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", *(*(ptr + i) + j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
